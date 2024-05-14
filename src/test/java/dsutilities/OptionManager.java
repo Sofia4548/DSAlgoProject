a231162de2807b3d220bcc3705c6649aff2f0955
Anju Kumari <anjuk1125@gmail.com>
2024-05-14 08:52:54 -0400

    test done

--- src/test/java/dsutilities/OptionManager.java
+++ src/test/java/dsutilities/OptionManager.java
@@ -10,7 +10,7 @@ public class OptionManager {
 public ChromeOptions getChromeOptions()
 {
 	ChromeOptions options = new ChromeOptions();
-	options.addArguments("--headless");
+//	options.addArguments("--headless");
 	options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
 	
 	return options;
@@ -18,7 +18,7 @@ public ChromeOptions getChromeOptions()
 public FirefoxOptions getFireFoxOptions()
 {
 	FirefoxOptions options = new FirefoxOptions();
-	options.addArguments("--headless");
+//	options.addArguments("--headless");
 	options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
 	
 	return options;
@@ -26,7 +26,7 @@ public FirefoxOptions getFireFoxOptions()
 public EdgeOptions getEdgeOptions()
 {
 	EdgeOptions options = new EdgeOptions();
-	options.addArguments("--headless");
+//	options.addArguments("--headless");
 	options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
 	return options;
 }
